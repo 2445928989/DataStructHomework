4801e2702794ea7aa1ae6a6dbd0a15bcab677a55
SGSS <2445928989@qq.com>
2025-04-28 16:59:42 +0800

    Updated.

--- DataStuctHomework/Edge.cpp
+++ DataStuctHomework/Edge.cpp
@@ -5,7 +5,7 @@
 std::map<unsigned int, Edge*> Edge::Edge_map;
 std::map<unsigned int, std::set<Edge>> Edge::graph;
 
-Edge::Edge() {}
+Edge::Edge():p1(nullptr),p2(nullptr),n(0),v(0),id(0) {}
 
 Edge::Edge(Point* _p1, Point* _p2):p1(_p1),p2(_p2) {}
 
